6cec086195107b05f654bb126ca2378af93111ed
Adam <adam6056@wp.pl>
2023-09-15 07:31:49 +0200

    Init repository.

---
+++ src/main/java/amkz/com/Promximity/PromximityApplication.java
@@ -0,0 +1,13 @@
+package amkz.com.Promximity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromximityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromximityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amkz/com/Promximity/PromximityApplicationTests.java
@@ -0,0 +1,13 @@
+package amkz.com.Promximity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromximityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
